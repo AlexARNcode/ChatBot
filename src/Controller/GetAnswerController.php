9b07ed742ab47be27967ad329616e1c8b062809d
AlexARNcode <alexarnout@gmail.com>
2022-03-07 16:09:39 +0100

    Chatbot: first commit

---
+++ src/Controller/GetAnswerController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Answers;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class GetAnswerController extends AbstractController
+{
+    #[Route('/getAnswer', name: 'app_get_answer')]
+    public function index(ManagerRegistry $doctrine, RequestStack $requestStack): Response
+    {
+        $userMessage = ($requestStack->getCurrentRequest()->query->get('message'));
+        $answer = $doctrine->getRepository(Answers::class)->getAnswerWhereQuestionisLike($userMessage);
+
+        if (empty($answer)) {
+            return $this->json([
+                'answer' => null
+            ]);
+        }
+
+        return $this->json([
+            'answer' => $answer[0]->getAnswer(),
+        ]);
+    }
+}
---
+++ src/Entity/Answers.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AnswersRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=AnswersRepository::class)
+ */
+class Answers
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $message;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $answer;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(string $message): self
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    public function getAnswer(): ?string
+    {
+        return $this->answer;
+    }
+
+    public function setAnswer(string $answer): self
+    {
+        $this->answer = $answer;
+
+        return $this;
+    }
+}
