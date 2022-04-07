2d51a264cf34da19613501fcf70598e8483fd989
AlexARNcode <alexarnout@gmail.com>
2022-04-07 17:01:59 +0200

    Add fixtures (questions and answers)

---
+++ src/DataFixtures/QuestionsAndAnswersFixtures.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Answers;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class QuestionsAndAnswersFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $answerEntity = new Answers();
+        $answerEntity->setMessage('En quelle année est né Léonard Da Vinci ?');
+        $answerEntity->setAnswer('Léonard Da Vinci est né le 14 avril 1452');
+        $manager->persist($answerEntity);
+        
+        $answerEntity = new Answers();
+        $answerEntity->setMessage('Qui a inventé la Montgolfière ?');
+        $answerEntity->setAnswer('Joseph-Michel et Jacques-Étienne Montgolfier ont inventé la Montgolfière.');
+        $manager->persist($answerEntity);
+        $manager->flush();
+    }
+}
