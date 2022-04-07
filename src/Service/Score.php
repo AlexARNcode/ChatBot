d7b62d1501957e7fbd043d009edc6b9e8fa934f3
AlexARNcode <alexarnout@gmail.com>
2022-04-07 14:52:40 +0200

    Simplify 'Score' service code

--- src/Service/Score.php
+++ src/Service/Score.php
@@ -4,19 +4,14 @@ namespace App\Service;
 
 class Score
 {
-    public function getHighestScore($questionsAndAnswers, $userQuestion)
+    public function getHighestScore($questionsAndAnswers, $userQuestion): array
     {
-        $highestScore = 0;
-
         foreach ($questionsAndAnswers as $questionsAndAnswer) {
             $currentScore =  similar_text(
                 $questionsAndAnswer->getMessage(),
                 $userQuestion,
                 $similarityScoreInPercentage
             );
-
-            if ($currentScore > $highestScore) {
-                $highestScore = $currentScore;
 
                 unset($scoreResults);
 
@@ -26,9 +21,11 @@ class Score
                         'answer' => $questionsAndAnswer->getAnswer(),
                         'score' => $similarityScoreInPercentage
                     ];
-            }
+            
         }
 
-        return array_reduce($scoreResults, 'array_merge', array());
+        return array_reduce(
+            $scoreResults, 'array_merge', array()
+        );
     }
 }
