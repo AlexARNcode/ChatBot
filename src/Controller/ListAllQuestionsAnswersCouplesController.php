a8a35c7596ffcae9913a476851562b305f04fc60
AlexARNcode <alexarnout@gmail.com>
2022-04-20 12:45:34 +0200

    Update List All Questions Answers Couples controller

--- src/Controller/ListAllQuestionsAnswersCouplesController.php
+++ src/Controller/ListAllQuestionsAnswersCouplesController.php
@@ -4,25 +4,33 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Doctrine\Persistence\ManagerRegistry;
 use App\Entity\Answers;
-use Symfony\Component\HttpFoundation\JsonResponse;
+
 
 class ListAllQuestionsAnswersCouplesController extends AbstractController
 {
-    #[Route('/questions-answers-couples', name: 'list_questions_and_answers_couples', methods: ['GET'])]
-    public function index(ManagerRegistry $doctrine): JsonResponse
+    #[Route(
+        '/questions-answers-couples', 
+        name: 'list_questions_and_answers_couples', 
+        methods: ['GET'])
+    ]
+    public function index(ManagerRegistry $doctrine)
     {
-        $allQuestionsAndAnswers = $doctrine->getRepository(Answers::class)->findAll();
-
-        foreach ($allQuestionsAndAnswers as $questionAndAnswerKey => $questionAndAnswerValues) {
-            $response[$questionAndAnswerValues->getId()] = [
-                $questionAndAnswerValues->getMessage() => $questionAndAnswerValues->getAnswer()
-            ];
-        }
-
-        return new JsonResponse($response);
+        $em = $doctrine->getManager();
+        $query = $em->createQuery(
+            'SELECT a
+            FROM App\Entity\Answers a'
+        );
+        $allQuestionsAndAnswers = $query->getArrayResult();
+    
+        $response = new Response(json_encode($allQuestionsAndAnswers));
+        $response->headers->set('Content-Type', 'application/json');
+    
+        return $response;
+    
     }
 }
